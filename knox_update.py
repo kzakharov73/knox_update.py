4b7e58ef953b50dce5be7fbb70c5d6c5c3308cef
bashwhacker <n249vt@gmail.com>
2024-03-01 15:21:08 +0300

    driver autoupdate, set colors

--- knox_update.py
+++ knox_update.py
@@ -1,62 +1,83 @@
-import sys
+import os
 import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as Eco
 from selenium.webdriver.support.wait import WebDriverWait
-from webdriver_auto_update import check_driver
 import pyautogui
+import pyfiglet
+from colorama import init, Fore, Back, Style
+from webdriver_auto_update.chrome_app_utils import ChromeAppUtils
+from webdriver_auto_update.webdriver_manager import WebDriverManager
 
-check_driver('C:\WINDOWS\system32')
+
+privet = pyfiglet.figlet_format('KNOX ROBOT')
+print(Style.BRIGHT + Fore.GREEN)
+print(privet)
+# Using ChromeAppUtils to inspect Chrome application version
+chrome_app_utils = ChromeAppUtils()
+
+
+# Target directory to store chromedriver
+driver_directory = "C:\WINDOWS\system32"
+
+# Create an instance of WebDriverManager
+driver_manager = WebDriverManager(driver_directory)
+
+# Call the main method to manage chromedriver
+driver_manager.main()
+
+dir_path = os.getcwd()
+user_profile_path = os.environ['USERPROFILE']
+user_profile_path.replace('/', '\\\\')
+
 with open('knox.txt', 'r') as f:
     for line in f:
         line = line.strip()
         login, password, domain, timeout = line.split('::')
 
-print('timeout = ', timeout)
-timeout = round(int(timeout) / 2)
+print(Fore.RED, 'timeout = ', timeout)
+timeout = round(int(timeout)/2)
+
+
 def mouse():
     pyautogui.moveRel(100, 0, duration=0.01)
     pyautogui.moveRel(0, 100)
     pyautogui.moveRel(-100, 0)
     pyautogui.moveRel(0, -100)
+
+
 def knox_Update():
     global tr_count
+
     options = Options()
-    # options.add_argument("--headless")  # Runs Chrome in headless mode.
-    options.add_argument('--no-sandbox')  # Bypass OS security model
-    # options.add_argument('--disable-gpu')  # applicable to windows os only
-    options.add_argument('start-maximized')
-    options.add_argument('disable-infobars')
-    options.add_argument('--disable-extensions')
-    options.add_experimental_option("prefs", {
-        "safebrowsing_for_trusted_sources_enabled": False,
-        "safebrowsing.enabled": False
-    })
-    options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    options.add_argument("--user-data-dir=" + user_profile_path + '\\AppData\\Local\\Google\\Chrome\\User Data')
     driver = webdriver.Chrome(options=options)
     wait = WebDriverWait(driver, 20)
     try:
         driver.get(domain)
         time.sleep(4)
     except:
-        print('ошибка - не открылся браузер')
+        print('chrome not open')
         tr_count += 1
         try:
             driver.stop_client()
             driver.close()
             driver.quit()
         except:
-            print('браузер не закрыт')
+            print('chrome not close')
 
         return tr_count
     time.sleep(2)
 
     try:
-        driver.find_element(By.XPATH, "/html/body/div[6]/div/button[2]").click()
+        # driver.find_element(By.XPATH, "/html/body/div[6]/div/button[2]").click()
+        driver.find_element(By.ID, "USERID").clear()
         driver.find_element(By.ID, "USERID").send_keys(login)
+        driver.find_element(By.ID, "USERPASSWORD").click()
         driver.find_element(By.ID, "USERPASSWORD").send_keys(password)
+        time.sleep(10)
         driver.find_element(By.XPATH, "/html/body/div[3]/div/form/div/fieldset/ul/li[4]/button/span").click()
         wait.until(Eco.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div[3]/div[2]/div[1]/ul/li[4]/a")))
         print("login_by", login, "\nWith password")
@@ -66,7 +87,7 @@ def knox_Update():
             print("login_by_cookies")
         except:
             tr_count += 1
-            print('ошибка при авторизации')
+            print('authorization error ')
             driver.stop_client()
             driver.close()
             driver.quit()
@@ -77,7 +98,7 @@ def knox_Update():
             driver.find_element(By.XPATH, "/html/body/div[1]/div[8]/div[1]/button").click()
         except:
             print('no notification')
-        print('попытка = ', a)
+        print(Fore.BLUE, 'try_count = ', a)
         try:
             c = 0
             while c < 8:
@@ -90,7 +111,7 @@ def knox_Update():
                 time.sleep(int(timeout))
                 driver.find_element(By.XPATH, "/html/body/div[1]/div[3]/div[2]/div[1]/ul/li[3]/a").click()
                 c += 1
-                print('повтор = ', c)
+                print('retry = ', c)
 
             driver.find_element(By.XPATH, "/html/body/div[1]/div[3]/div[1]/div[6]/div[2]/span/img").click()
             time.sleep(2)
@@ -112,19 +133,15 @@ def knox_Update():
         tr_count += 1
         return tr_count
     except:
-        print('браузер не закрыт')
+        print('chrome do not close')
         tr_count += 1
         return tr_count
-
 
 
 tr_count = 0
 
 while tr_count < 3:
-    print('перезагрузка браузера =', tr_count)
+    print(Fore.MAGENTA, 'chrome reboot =', tr_count)
     knox_Update()
 else:
-    print('ошибка, количество попыток = ', tr_count)
-
-
-
+    print(Fore.RED, 'error, try counter = ', tr_count)
